7fe17d7825e1285a3bf201abce39ce8f80c709b7
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-06-29 14:35:02 +0300

    ex4

---
+++ src/Ex4.java
@@ -0,0 +1,9 @@
+public class Ex4 {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 2;
+        double c = ((double)a) / b;
+
+        System.out.printf("%.3f\n", c);
+    }
+}
